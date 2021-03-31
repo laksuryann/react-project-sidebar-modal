0ca571ad245189bf62fc7475692530c188f8f233
venkatesh dharmavarapu <venkatadharma1@gmail.com>
2021-03-31 18:09:59 +0530

    Modal.js is added

---
+++ src/Modal.js
@@ -0,0 +1,26 @@
+import React,{useContext} from 'react';
+import { useGlobalContext } from './context';
+import { FaTimes } from 'react-icons/fa';
+
+
+const Modal = () => {
+
+  const contextStore= useGlobalContext
+  const { isModalOpen, closeModal } = contextStore;
+  return (
+    <div
+      className={`${
+        isModalOpen ? 'modal-overlay show-modal' : 'modal-overlay'
+      }`}
+    >
+      <div className='modal-container'>
+        <h3>modal content</h3>
+        <button className='close-modal-btn' onClick={closeModal}>
+          <FaTimes></FaTimes>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
